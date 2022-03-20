bfeacc4a04710eb355aaf7172d6828e3c88718b1
Sodiq Olalekan <ganiyusodiq059@gmail.com>
2022-03-20 12:03:46 -0600

    alternatingSum challenge

---
+++ src/alternatingSums/alternatingSums.ts
@@ -0,0 +1,16 @@
+export function alternatingSums(a: number[]): number[] {
+  let oddSum: number = 0;
+  let evenSum: number = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    if (i % 2 !== 0) {
+      oddSum += a[i];
+    }
+    if (i % 2 === 0) {
+      evenSum += a[i];
+    }
+  }
+  return [evenSum, oddSum];
+}
+
+console.log(alternatingSums([50, 60, 60, 45, 70]));
